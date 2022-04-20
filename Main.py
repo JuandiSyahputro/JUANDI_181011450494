86a116b5c3bfcb863068bc62d9472bbe4eeeb2e3
juandi <juandi.syahputra.tsm1@gmail.com>
2022-04-21 04:39:11 +0700

    commit pertama

---
+++ Main.py
@@ -0,0 +1,19 @@
+nama = print("Daftar list minuman")
+nama = print("1.Kopi Rp.4000")
+nama = print("2.Teh Rp. 3000")
+nama = print("3.Susu Rp.5000")
+nama = input ("apa yang mau anda pesan?   ")
+
+
+if nama == "1":
+  print("Terimakasih sudah memesan")
+  print ("Pesananmu akan segera di siapkan")
+elif nama == "2":
+  print ("Terimakasih sudah memesan")
+  print("Pesananmu akan segera kami siapkan")
+elif nama == "3":
+  print("Terimakasih sudah memesan")
+  print("Pesananmu akan segera kami siapkan")
+else:
+  ("Maaf pesanan yang anda masukan tidak benar!")
+
